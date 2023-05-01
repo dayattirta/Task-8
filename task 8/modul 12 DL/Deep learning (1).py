d6bc9e2e729e8720166f450af8873160ad7af1d6
dayattirta <127614366+dayattirta@users.noreply.github.com>
2023-05-01 20:55:17 +0700

    Add files via upload

---
+++ task 8/modul 12 DL/Deep learning (1).py
@@ -0,0 +1,2 @@
+!wget --no-check-certificate \
+    https:dicodingacademy.blob.core.windows.net/picodiploma/ml_pemula_academy/rockpaperscissors.zip
---
+++ task 8/modul 12 DL/Deep learning (2).py
@@ -0,0 +1,10 @@
+
+import zipfile,os
+local_zip = 'rockpaperscissors.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('file_extracted')
+zip_ref.close()
+
+from sklearn.model_selection import train_test_split
+base_dir = 'file_extracted/rockpaperscissors/rps_cv_images'
+os.listdir(base_dir)
